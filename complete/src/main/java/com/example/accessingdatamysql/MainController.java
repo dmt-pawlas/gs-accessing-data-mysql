6c770572c116b65caf67c49c43632924e4111611
dmt.jola <dmt.jola@lo5.bielsko.pl>
2023-04-15 12:38:24 +0200

    Added new commit spring

--- complete/src/main/java/com/example/accessingdatamysql/MainController.java
+++ complete/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ch.qos.logback.core.joran.conditional.ElseAction;
+
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
@@ -21,15 +23,23 @@ public class MainController {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
-		User n = new User();
+		
+		Jola_User n = new Jola_User();
 		n.setName(name);
 		n.setEmail(email);
+
+		if (userRepository.findByEmail(email) == null);
 		userRepository.save(n);
 		return "Saved";
+		else
+		return 
+		
+
+
 	}
 
 	@GetMapping(path="/all")
-	public @ResponseBody Iterable<User> getAllUsers() {
+	public @ResponseBody Iterable<Jola_User> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
--- complete/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ complete/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -2,11 +2,13 @@ package com.example.accessingdatamysql;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.accessingdatamysql.User;
+import com.example.accessingdatamysql.Jola_User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<Jola_User, Integer> {
 
+
+    Jola_User findByEmail (String email);
 }
